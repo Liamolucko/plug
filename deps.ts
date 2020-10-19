349f30db1ba1b1f4155baed6426b456f3269081c
Liam Murphy <43807659+Liamolucko@users.noreply.github.com>
2020-10-19 18:08:39 +1100

    use fork

--- deps.ts
+++ deps.ts
@@ -8,4 +8,4 @@ export { ensureDir, exists } from "https://deno.land/std@0.74.0/fs/mod.ts";
 
 export { createHash } from "https://deno.land/std@0.74.0/hash/mod.ts";
 
-export { Cache } from "https://deno.land/x/cache@0.2.3/mod.ts";
+export { Cache } from "https://denopkg.com/Liamolucko/cache@main/mod.ts";
